56eeecf99f961f7fe204794116e6c69d13caa43e
Thomas Nast <thomas.nast23@gmail.com>
2025-09-09 20:47:10 -0700

    Add ability to write data to parquet files

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from modules.alphavantage_query import AlphavantageQuery
+
+load_dotenv()
+api_key = os.getenv('ALPHAVANTAGE_API_KEY')
+url_base = os.getenv('ALPHAVANTAGE_URL_BASE')
+data_path = os.getenv('DATA_PATH')
+
+if os.path.exists(data_path):
+    alphavantage_query = AlphavantageQuery(api_key, url_base)
+    alphavantage_query.add_function_to_url("TIME_SERIES_INTRADAY").add_symbol_to_url("IBM").add_interval_to_url("5min").add_api_key_to_url(api_key).write_to_parquet(data_path)
+else:
+    print('no data path found')
--- modules/alphavantage_query.py
+++ modules/alphavantage_query.py
@@ -1,28 +1,91 @@
 
 import os
+import pandas as pd
+import requests
 
 class AlphavantageQuery:
     def __init__(self, api_key, url_base):
+        '''
+        Initialize the AlphavantageQuery class. Used with the Alphavantage API.
+        https://www.alphavantage.co/documentation/
+        Args:
+            api_key: The api key for the Alphavantage API.
+            url_base: The base url for the Alphavantage API.
+        Returns:
+            None
+        '''
         self.api_key = api_key
         self.target_url = url_base
-
+        self.function = None
+        self.symbol = None
+        self.interval = None
+    #region url building
     def add_function_to_url(self, function):
+        '''
+        Add the function to the url.
+        '''
+        self.function = function
         self.target_url = f"{self.target_url}?function={function}"
         return self
 
     def add_symbol_to_url(self, symbol):
+        '''
+        Add the symbol to the url.
+        '''
+        self.symbol = symbol
         self.target_url = f"{self.target_url}&symbol={symbol}"
         return self
 
     def add_interval_to_url(self, interval):
+        '''
+        Add the interval to the url.
+        '''
+        self.interval = interval
         self.target_url = f"{self.target_url}&interval={interval}"
         return self
 
     def add_api_key_to_url(self, api_key):
+        '''
+        Add the api key to the url.
+        '''
         self.target_url = f"{self.target_url}&apikey={api_key}"
         return self
+    #endregion
+    #region url modification
+    def alter_url_function(self, function):
+        '''
+        Alter the function parameter of the url.
+        '''
+        self.function = function
+        self.target_url = self.target_url.replace(f"function={self.function}", f"function={function}")
+        return self
+    
+    def alter_url_symbol(self, symbol):
+        '''
+        Alter the symbol parameter of the url.
+        '''
+        self.symbol = symbol
+        self.target_url = self.target_url.replace(f"symbol={self.symbol}", f"symbol={symbol}")
+        return self
+
+    def alter_url_interval(self, interval):
+        '''
+        Alter the interval parameter of the url.
+        '''
+        self.interval = interval
+        self.target_url = self.target_url.replace(f"interval={self.interval}", f"interval={interval}")
+        return self  
+    #endregion
     
     def print_target_url(self):
         print(self.target_url)
+        return self
+
+    def get_data(self):
+        return requests.get(self.target_url).json()
+
+    def write_to_parquet(self, path):
+        df = pd.DataFrame(self.get_data())
+        df.to_parquet(f"{path}/{self.symbol}.parquet")
         return self
     
