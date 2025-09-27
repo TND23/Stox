5dde0870fd3bb39e2d4016ce3d3eb04012c61a2b
Thomas Nast <thomas.nast23@gmail.com>
2025-09-27 16:36:17 -0700

    test alphavantage

--- modules/alphavantage_query.py
+++ modules/alphavantage_query.py
@@ -2,6 +2,16 @@
 import os
 import pandas as pd
 import requests
+from enum import StrEnum
+
+class AlphavantageFunction(StrEnum):
+    '''
+    Enum for the functions of the Alphavantage API.
+    '''
+    News_Sentiment = "NEWS_SENTIMENT"
+    Historical_Options = "HISTORICAL_OPTIONS"
+    Exp_Moving_avg = "EMA"
+    Wgt_Moving_avg = "WMA"
 
 class AlphavantageQuery:
     def __init__(self, api_key, url_base):
@@ -20,12 +30,12 @@ class AlphavantageQuery:
         self.symbol = None
         self.interval = None
     #region url building
-    def add_function_to_url(self, function):
+    def add_function_to_url(self, function: AlphavantageFunction):
         '''
         Add the function to the url.
         '''
         self.function = function
-        self.target_url = f"{self.target_url}?function={function}"
+        self.target_url = f"{self.target_url}?function={function.value}"
         return self
 
     def add_symbol_to_url(self, symbol):
@@ -81,11 +91,21 @@ class AlphavantageQuery:
         print(self.target_url)
         return self
 
+    async def async_get_data(self):
+        '''
+        Get the data from the url asynchronously.
+        '''
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.target_url) as response:
+                return await response.json()
+
     def get_data(self):
+        '''
+        Get the data from the url synchronously.
+        '''
         return requests.get(self.target_url).json()
 
     def write_to_parquet(self, path):
         df = pd.DataFrame(self.get_data())
         df.to_parquet(f"{path}/{self.symbol}.parquet")
         return self
-    
